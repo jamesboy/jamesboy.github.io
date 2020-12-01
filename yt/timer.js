585eff5a9eaf8e04d63ec11b853a42d552eb37ab
jamesboy <jamesboy@users.noreply.github.com>
2020-12-01 08:47:58 +0100

    Add files via upload

---
+++ yt/timer.js
@@ -0,0 +1,35 @@
+var startButton = document.getElementById("startButton");
+var resetButton = document.getElementById("resetButton");
+var display = document.getElementById("time");
+var counter = 0;
+var myInterval = -1;
+
+startButton.addEventListener("click", function() {
+    if (myInterval == -1) {
+        startButton.innerHTML = "Pause";
+        myInterval = setInterval(count, 1000);
+    }
+    else {
+        startButton.innerHTML = "Start";
+        clearInterval(myInterval);
+        myInterval = -1;
+    }
+});
+
+resetButton.addEventListener("click", function(){
+    startButton.innerHTML = "Start";
+    clearInterval(myInterval);
+    myInterval = -1;
+    display.innerHTML = "00:00";
+    counter = 0;
+})
+
+
+function count() {
+    counter++;
+    var minutes = parseInt(counter / 60);
+    var seconds = (counter % 60 == 60) ? 0 : counter % 60;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    display.innerHTML = minutes + ':' + seconds;
+}
