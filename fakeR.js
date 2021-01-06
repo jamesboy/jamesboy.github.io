f90d24e20daddd7bf0dca36e518dafc99e10cb92
jamesboy <jamesboy@users.noreply.github.com>
2021-01-06 11:25:15 +0100

    Update fakeR.js

--- fakeR.js
+++ fakeR.js
@@ -1,3 +1,6 @@
-var prix = document.getElementsByClassName("ui large header")[0];
-prix.innerHTML = "$405.99<span>*</span>";
-console.log("done");
+var timer = setTimeout(doIt, 10000);
+function doIt() {
+  var prix = document.getElementsByClassName("ui large header")[0];
+  prix.innerHTML = "$405.99<span>*</span>";
+  console.log("done");
+}
